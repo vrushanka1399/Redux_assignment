58b6d22f3e3f345e3bb294f8adac9ff784eb1553
vrushanka1399 <vrushanka.1si17cs132@gmail.com>
2026-01-19 10:17:22 +0530

    Create counterReducer.js

---
+++ counterReducer.js
@@ -0,0 +1,14 @@
+function counterReducer(state = 0, action) {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+
+    case "decrement":
+      return state - 1;
+
+    default:
+      return state;
+  }
+}
+
+export default counterReducer;
